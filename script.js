c57add5cf52096f43df98f22fa2b7f129f52aa67
Gloria Jean <gcuradog01@iesalbarregas.es>
2025-11-26 02:22:49 +0100

    🔄 Cargar carburantes según provincia y municipio | script.js

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ async function cargarProvincias() {
 
     const select = document.getElementById("provincia");
 
+    //Limpiar antes de agregar
+    select.innerHTML = "<option value=''>Seleccione provincia</option>";
+
     console.log(data);
 
     data.forEach(provincia => {
@@ -26,6 +29,9 @@ async function cargarMunicipios(idProvincia) {
 
     const select = document.getElementById("municipio");
 
+    //Limpiar antes de agregar
+    select.innerHTML = "<option value=''>Seleccione municipio</option>";
+
     console.log(data);
 
     data.forEach(municipio => {
@@ -36,13 +42,95 @@ async function cargarMunicipios(idProvincia) {
     });
 }
 
+// 3. CARGAR GASOLINERAS SEGÚN PROVINCIAS
+async function cargarGasolinerasProv(idProvincia) {
+    const respuestaAPI = await fetch(base + "/EstacionesTerrestres/FiltroProvincia/" + idProvincia);
+    const data = await respuestaAPI.json();
+    console.log(data);
+
+    //Dentro de la data(es decir, el json) hay una lista con las gasolineras llamada ListaEESSPrecio
+    const lista = data.ListaEESSPrecio;
+    //Hago un set para que no haya valores duplicados
+    const setCarburante = new Set();
+
+    lista.forEach(gasolinera => {
+        for (let element in gasolinera) {
+            //En el json, los carburantes empiezan siempre por Precio ... (Gasolina 95)
+            if (element.startsWith("Precio ") && gasolinera[element] !== "") {
+                //Quito el principio "precio" para que aparezca solo el nombre del carburante en el select
+                setCarburante.add(element.replace("Precio ", ""));
+            }                                     
+        }
+    });
+
+    const select = document.getElementById("carburante");
+
+    //Limpiar antes de agregar
+    select.innerHTML = "<option value=''>Seleccione carburante</option>";
+
+    setCarburante.forEach(carburante => {
+        const opcionCarburante = document.createElement("option");
+        opcionCarburante.value = carburante;
+        opcionCarburante.textContent = carburante;
+        select.appendChild(opcionCarburante);
+    })
+    
+
+}
+
+// 4. CARGAR GASOLINERAS SEGÚN MUNICIPIOS
+async function cargarGasolinerasMun(IDMunicipio) {
+    const respuestaAPI = await fetch(base + "/EstacionesTerrestres/FiltroMunicipio/" + IDMunicipio);
+    const data = await respuestaAPI.json();
+    console.log(data);
+
+    //Dentro de la data(es decir, el json) hay una lista con las gasolineras llamada ListaEESSPrecio
+    const lista = data.ListaEESSPrecio;
+    //Hago un set para que no haya valores duplicados
+    const setCarburante = new Set();
+
+    lista.forEach(gasolinera => {
+        for (let element in gasolinera) {
+            //En el json, los carburantes empiezan siempre por Precio ... (Gasolina 95)
+            if (element.startsWith("Precio ") && gasolinera[element]) {
+                //Quito el principio "precio" para que aparezca solo el nombre del carburante en el select
+                setCarburante.add(element.replace("Precio ", ""));
+            }                                     
+        }
+    });
+
+    const select = document.getElementById("carburante");
+
+    //Limpiar antes de agregar
+    select.innerHTML = "<option value=''>Seleccione carburante</option>";
+
+    setCarburante.forEach(carburante => {
+        const opcionCarburante = document.createElement("option");
+        opcionCarburante.value = carburante;
+        opcionCarburante.textContent = carburante;
+        select.appendChild(opcionCarburante);
+    })
+}
+
 
 // EVENTOS
 document.getElementById("provincia").addEventListener("change", event => {
     // event.target.value es el ID de la provincia seleccionada
-    // Si hay provincia seleccionada -> carga los municipios de esa provincia
-    if (event.target.value) cargarMunicipios(event.target.value);
+    // Si hay provincia seleccionada -> carga los municipios, los carburantes y las gasolineras de esa provincia 
+    if (event.target.value) {
+        cargarMunicipios(event.target.value);
+        cargarGasolinerasProv(event.target.value);
+    }
 });
+
+document.getElementById("municipio").addEventListener("change", event => {
+    // event.target.value es el ID del municipio seleccionado
+    // Si hay municipio seleccionado -> carga los carburantes y las gasolineras de ese municipio 
+    if (event.target.value) {
+        cargarGasolinerasMun(event.target.value);
+    }
+});
+
 
 
 // INICIO
